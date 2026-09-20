252393296e43c8d9421c4b62dffbc88d2e436abf
Ruiseki <92140543+Shigure-Ruiseki@users.noreply.github.com>
2026-09-20 15:40:44 +0700

    🔥(refactor)(plugins): Remove NEI key event handler and ProjectE dependency (#38)
    ✨(fix)(modsupport): Correct ProjectE modid to 'ProjectE' for container lookup

--- src/main/java/ruiseki/jfmuy/proxy/CommonProxy.java
+++ src/main/java/ruiseki/jfmuy/proxy/CommonProxy.java
@@ -2,8 +2,6 @@ package ruiseki.jfmuy.proxy;
 
 import net.minecraftforge.common.MinecraftForge;
 
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.common.Loader;
 import ruiseki.jfmuy.JFMUY;
 import ruiseki.jfmuy.JFMUYHandler;
 import ruiseki.jfmuy.network.PacketCheatPermission;
@@ -13,7 +11,6 @@ import ruiseki.jfmuy.network.PacketGiveItemStack;
 import ruiseki.jfmuy.network.PacketRecipeTransfer;
 import ruiseki.jfmuy.network.PacketRequestCheatPermission;
 import ruiseki.jfmuy.network.PacketSetHotbarItemStack;
-import ruiseki.jfmuy.plugins.nei.NEIKeyEventHandler;
 import ruiseki.okcore.init.ModBase;
 import ruiseki.okcore.network.PacketHandler;
 import ruiseki.okcore.proxy.CommonProxyComponent;
@@ -41,10 +38,5 @@ public class CommonProxy extends CommonProxyComponent {
     public void registerEventHooks() {
         super.registerEventHooks();
         MinecraftForge.EVENT_BUS.register(JFMUYHandler.INSTANCE);
-        if (Loader.isModLoaded("NotEnoughItems")) {
-            FMLCommonHandler.instance()
-                .bus()
-                .register(new NEIKeyEventHandler());
-        }
     }
 }
